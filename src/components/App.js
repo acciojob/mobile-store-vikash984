615d730a3d8aeaaa67950ac0e3ab3ffcd4500ce2
vikash984 <vikashk15072001@gmail.com>
2025-08-03 21:59:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,17 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import ProductList from "./components/ProductList";
-import ProductDetails from "./components/ProductDetails";
-import AdminPanel from "./components/AdminPanel";
-import { initialProducts } from "./data";
-import "./styles.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProductList from './pages/ProductList';
+import ProductDetails from './pages/ProductDetails';
+import AdminPanel from './pages/AdminPanel';
 
 function App() {
-  const [products, setProducts] = useState(initialProducts);
-
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/admin">Admin Panel</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<ProductList products={products} />} />
-        <Route path="/products/:id" element={<ProductDetails products={products} />} />
-        <Route
-          path="/admin"
-          element={<AdminPanel products={products} setProducts={setProducts} />}
-        />
-      </Routes>
+      <Switch>
+        <Route path="/" exact component={ProductList} />
+        <Route path="/products/:id" component={ProductDetails} />
+        <Route path="/admin" component={AdminPanel} />
+      </Switch>
     </Router>
   );
 }
